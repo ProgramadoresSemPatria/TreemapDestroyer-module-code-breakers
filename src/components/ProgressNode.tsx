f3b8452a93c6c1364318f46a20427e841a497f36
Carlos Henryck <ryck302@gmail.com>
2024-12-07 02:06:05 -0300

    feat: add progress bar on node

--- src/components/ProgressNode.tsx
+++ src/components/ProgressNode.tsx
@@ -4,13 +4,16 @@ type ProgressNode = Node<{ label: string }>;
 
 export function ProgressNode({ data }: NodeProps<ProgressNode>) {
   return (
-    <button className="flex items-center justify-center rounded-md w-56 py-4 bg-[#874EF9] text-white">
+    <button className="flex flex-col items-center gap-2 px-4 justify-center rounded-md w-56 py-4 bg-[#874EF9] hover:filter">
       <Handle
         type="target"
         position={Position.Top}
         className="bg-transparent border-0"
       />
-      {data.label}
+      <p className="text-white">{data.label}</p>
+      <div className="w-full h-2 bg-[#EFEAFA] rounded-sm overflow-hidden">
+        <div className="w-1/2 h-full bg-[#32EEB4] transition-[height] duration-500" />
+      </div>
       <Handle
         type="source"
         position={Position.Bottom}
