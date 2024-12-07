5116ac036ae2a122900c2e02238c000bf50adc27
Carlos Henryck <ryck302@gmail.com>
2024-12-06 23:06:43 -0300

    feat: finish buttons style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        "left-to-right":
+          "left-to-right 1s cubic-bezier(0.165, 0.84, 0.44, 1) backwards",
+      },
+      keyframes: {
+        "left-to-right": {
+          "0%": { transform: "translateX(-200%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
